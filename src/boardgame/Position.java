459e883e74ee5a53c2cc12ad67864483f435b18f
Daniel Souza <daniel.f.souza@hotmail.com>
2019-06-08 00:38:50 -0300

    Rook moves

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -1,14 +1,14 @@
 package boardgame;
 
 public class Position {
-	
+
 	private int row;
 	private int column;
-	
+
 	public Position() {
 
 	}
-	
+
 	public Position(int row, int column) {
 		this.row = row;
 		this.column = column;
@@ -29,12 +29,18 @@ public class Position {
 	public void setColumn(int column) {
 		this.column = column;
 	}
-	
+
+	public void setValues(int row, int column) {
+		this.row = row;
+		this.column = column;
+
+	}
+
 	@Override
 	public String toString() {
-		
+
 		return row + ", " + column;
-		
+
 	}
 
 }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -41,6 +41,7 @@ public class ChessMatch {
 		Position target = targetPosition.toPosition();
 
 		validateSourcePosition(source);
+		validateTargetPosition(source, target);
 
 		Piece capturedPiece = makeMove(source, target);
 
@@ -72,6 +73,17 @@ public class ChessMatch {
 		}
 
 	}
+	
+	private void validateTargetPosition(Position source, Position target) {
+		
+		if (!board.piece(source).possibleMove(target)) {
+			
+			throw new ChessException ("The chosen piece cannot move to target position.");
+			
+		}
+		
+		
+	}
 
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 
@@ -95,5 +107,7 @@ public class ChessMatch {
 		placeNewPiece('e', 8, new Rook(board, Color.BLACK));
 		placeNewPiece('d', 8, new King(board, Color.BLACK));
 	}
+	
+	
 
 }
